cededea312c04f4cc218045eb376e88060cb1c66
Madeleine Berner <mberne@kth.se>
2017-11-19 22:17:05 +0100

    Printing to screen done, can use println

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,36 @@
 #![feature(lang_items)]
+#![feature(const_fn)]
+#![feature(const_unique_new)]
+#![feature(unique)]
 #![no_std]
 
 extern crate rlibc;
+extern crate spin;
+extern crate volatile;
+
+#[macro_use]
+mod vga_buffer;
 
 #[no_mangle]
 pub extern fn rust_main() {
     // ATTENTION: we have a very small stack and no guard page
 
+    vga_buffer::clear_screen();
+    println!("Hello World{}", "!");
+    println!("{}", { println!("inner"); "outer" });
+    println!("No one puts thread in deadlock{}", "!");
+
+    /*
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello again");
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337);
+    */
+
+    /*
+    vga_buffer::print_something();
+    */
+
+    /*
     let hello = b"Hello World!";
     let color_byte = 0x1f; // white foreground, blue background
 
@@ -18,6 +42,7 @@ pub extern fn rust_main() {
     // write `Hello World!` to the center of the VGA text buffer
     let buffer_ptr = (0xb8000 + 1988) as *mut _;
     unsafe { *buffer_ptr = hello_colored };
+    */
 
     loop{}
 }
