fee3d044e9e5becdb3b073900707d830ecfb5b14
Madeleine Berner <mberne@kth.se>
2017-11-19 18:39:25 +0100

    Set up rust part 1 done, prints Hello World!

--- src/arch/x86_64/long_mode_init.asm
+++ src/arch/x86_64/long_mode_init.asm
@@ -11,6 +11,10 @@ long_mode_start:
 	mov fs, ax
 	mov gs, ax
 
+	;;  call the rust main
+	extern rust_main
+	call rust_main
+	
 	;;  print `Hej Johan :) ` to screen
 	mov rax, 0x5f203f6a3f653f48
 	mov qword [0xb8000], rax
---
+++ target/x86_64-flaming_os/debug/libflaming_os.d
@@ -0,0 +1,1 @@
+/home/madeleine/Documents/KTH/ID2206/mOS/flamingOS/target/x86_64-flaming_os/debug/libflaming_os.a: /home/madeleine/Documents/KTH/ID2206/mOS/flamingOS/src/lib.rs
